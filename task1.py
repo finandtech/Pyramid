6e14529809673fd5e22301161cd6fee285205bd1
emantech <emangf2000@gmail.com>
2021-03-15 20:31:29 -0600

    update

--- task1.py
+++ task1.py
@@ -1,20 +1,10 @@
 def makeNumberPyramid(num):
     lenght = len(str(num))
-    number = lenght * int((num + 1) / 2) + int((num + 1) / 2)
+    number = (lenght + 1) * num
     for i in range(1, num + 1):
-        if (i < 10):
-                for j in range(1, number - i):
-                    print(' ', end="")
-                for j in range(1, i + 1):
-                    print(i, end=" ")
-        else:
-            for j in range(1, lenght * num - lenght * i):
-                print(' ', end="")
-            for j in range(1, i + 1):
-                print(i, end=" ")
-        print()
-    print(str(num))
-
-
+        message = ""
+        for j in range(i):
+            message += str(i)+" "
+        print(format(message, "^"+str(number)))
 num = eval(input("Enter the number of rows: "))
 output = makeNumberPyramid(num)
